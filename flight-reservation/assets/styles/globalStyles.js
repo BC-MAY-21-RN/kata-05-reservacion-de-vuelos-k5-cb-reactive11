633282a3d298d67763c40d626d857ab0e2cac103
Wilson Lopez <wiltrouble@gmail.com>
2022-09-01 18:34:03 -0400

    fix imports, Co-authored-by: Astroyayo <lug044@gmail.com> Co-authored-by: TonyHdez-Pi <tonyhdezpi@gmail.com> Co-authored-by: AmarilloVel <hvelasco0@ucol.mx>

--- flight-reservation/assets/styles/globalStyles.js
+++ flight-reservation/assets/styles/globalStyles.js
@@ -1,4 +1,4 @@
-import { StyleSheet } from "react-native";
+import { Platform, StatusBar, StyleSheet } from "react-native";
 export const styles = StyleSheet.create({
     container: {
       flex: 1,
