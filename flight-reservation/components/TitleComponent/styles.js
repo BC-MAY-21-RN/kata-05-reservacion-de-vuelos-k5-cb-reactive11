03fe81307d126c3e0acdd67cda241c9238e1c6cb
Hector Velasco <hvelasco0@ucol.mx>
2022-09-05 18:06:44 -0500

    feat(TitleComponent):Implement standard title comp
    Co-authored-by: Astroyayo <lug044@gmail.com>

---
+++ flight-reservation/components/TitleComponent/styles.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    title: {
+        color: "#6270de",
+        fontWeight: "800",
+        fontSize: 24,
+        paddingVertical: 30,
+      },
+});
