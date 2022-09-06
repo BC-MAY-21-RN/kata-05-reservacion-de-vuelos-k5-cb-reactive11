3dd6b391ccc9d1f8290ebddfe0ebc4032abe6308
GerardBR3270 <lugo044@gmail.com>
2022-09-06 18:34:25 -0500

    refactor(FlightItem): add props to component
    also add identation to style file
    
    Co-authored-by: AmarilloVel <hvelasco0@ucol.mx>

--- flight-reservation/components/FlightItemComponent/style.js
+++ flight-reservation/components/FlightItemComponent/style.js
@@ -1,39 +1,41 @@
 import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
-    mainContainer:{
-        borderColor: '#000',
-        borderBottomWidth: 1,
-        paddingBottom: 20,
-        width: '80%'
-    },
-    destination:{
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        paddingBottom: 10,
-        marginBottom: 10,
-        borderBottomWidth: .55,
-        borderColor: '#808080'
-    },
-    date:{
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center'
-    },
-    destinationText:{
-        fontSize: 22,
-        fontWeight: 'bold'
-    },
-    dateText:{
-        fontSize: 15,
-        fontWeight: '600'
-    },
-    subText: {
-        fontSize: 12,
-        color: 'gray'
-    }
-
+  mainContainer: {
+    borderColor: "#000",
+    borderBottomWidth: 1,
+    paddingBottom: 20,
+  },
+  destination: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingBottom: 10,
+    marginBottom: 10,
+    borderBottomWidth: 0.55,
+    borderColor: "#808080",
+  },
+  rightColumn: {
+    alignItems: "flex-end",
+  },
+  date: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  destinationText: {
+    fontSize: 22,
+    fontWeight: "bold",
+  },
+  dateText: {
+    fontSize: 15,
+    fontWeight: "600",
+  },
+  subText: {
+    fontSize: 14,
+    fontWeight: "300",
+    color: "gray",
+  },
 });
 
 export default styles;
