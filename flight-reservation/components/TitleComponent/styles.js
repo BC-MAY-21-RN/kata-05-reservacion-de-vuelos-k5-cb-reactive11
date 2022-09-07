464bd0fe85eb5d4c680b78479a5083218a351967
Antonio de Jesus Hernandez Piña <tonyhdezpi@gmail.com>
2022-09-07 18:59:07 -0500

    Refactor TitleComponent
    changes where made to the styles file to make the letters bigger
    
    Co-authored-by: wiltrouble <wiltrouble@gmail.com>

--- flight-reservation/components/TitleComponent/styles.js
+++ flight-reservation/components/TitleComponent/styles.js
@@ -3,9 +3,10 @@ import { StyleSheet } from "react-native";
 const styles = StyleSheet.create({
   title: {
     color: "#6270de",
-    fontWeight: "800",
-    fontSize: 24,
-    paddingVertical: 30,
+    fontWeight: "900",
+    fontSize: 28,
+    paddingBottom: 10,
+    letterSpacing: .9
   },
 });
 
