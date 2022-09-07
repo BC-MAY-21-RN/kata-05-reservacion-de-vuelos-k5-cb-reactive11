32ae92e06e22eb3a9f481a97cdcae9d55773baf3
Wilson Lopez <wiltrouble@gmail.com>
2022-09-07 17:27:44 -0400

    fix app.js
    Co-authored-by: TonyHdez-Pi <tonyhdezpi@gmail.com>

--- flight-reservation/App.js
+++ flight-reservation/App.js
@@ -1,13 +1,11 @@
 import { SafeAreaView } from "react-native";
 import { SignUpScreen } from "./screens";
 import { styles } from './assets/styles/globalStyles'
-import LoginScreen from "./screens/LoginScreen";
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <LoginScreen />
-
+      <SignUpScreen />
     </SafeAreaView>
   );
 }
