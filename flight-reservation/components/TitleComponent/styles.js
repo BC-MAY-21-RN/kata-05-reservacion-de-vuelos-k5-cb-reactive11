11ddb5f51edbe249f5b572ff4864554f83d7ecb5
GerardBR3270 <lugo044@gmail.com>
2022-09-12 17:48:45 -0500

    feat(Title): implement a prop to change color
    Co-authored-by: wiltrouble <wiltrouble@gmail.com>

--- flight-reservation/components/TitleComponent/styles.js
+++ flight-reservation/components/TitleComponent/styles.js
@@ -1,13 +1,14 @@
 import { StyleSheet } from "react-native";
 
-const styles = StyleSheet.create({
-  title: {
-    color: "#6270de",
-    fontWeight: "900",
-    fontSize: 28,
-    paddingBottom: 10,
-    letterSpacing: .9
-  },
-});
+const styles = (textColor) =>
+  StyleSheet.create({
+    title: {
+      color: textColor,
+      fontWeight: "900",
+      fontSize: 28,
+      paddingBottom: 10,
+      letterSpacing: 0.9,
+    },
+  });
 
 export default styles;
