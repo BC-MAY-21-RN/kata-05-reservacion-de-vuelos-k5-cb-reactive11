8779e886532a28e47552e472e8fb046822b07b60
Hector Velasco <hvelasco0@ucol.mx>
2022-09-13 16:41:50 -0500

    Identation fixed
    Co-authored-by: TonyHdez-Pi <tonyhdezpi@gmail.com>

--- flight-reservation/components/SelectLocationComponent/index.js
+++ flight-reservation/components/SelectLocationComponent/index.js
@@ -1,25 +1,26 @@
 import React from "react";
-import { View,Text } from "react-native";
-import SelectList from 'react-native-dropdown-select-list'; 
+import { View, Text } from "react-native";
+import SelectList from "react-native-dropdown-select-list";
 import styles from "./styles";
-import countries from "../../assets/data/countries.json"
+import countries from "../../assets/data/countries.json";
 
-const SelectLocationComponent = () =>{
-   
-    const [selected, setSelected] = React.useState("");
+const SelectLocationComponent = () => {
+  const [selected, setSelected] = React.useState("");
 
+  return (
+    <View style={styles.container}>
+      <SelectList
+        placeholder="Select Location"
+        data={countries}
+        setSelected={setSelected}
+        boxStyles={styles.boxStyles}
+        dropdownStyles={styles.optionsBox}
+        inputStyles={styles.searchText}
+        searchicon=""
+      />
+      <View style={styles.line}></View>
+    </View>
+  );
+};
 
-    return(
-        <View style={styles.container}>
-            <SelectList placeholder="Select Location" 
-            data={countries} setSelected={setSelected} 
-            boxStyles={styles.boxStyles} 
-            dropdownStyles={styles.optionsBox} inputStyles={styles.searchText} 
-            searchicon=""/>
-            <View style={styles.line}></View>
-        </View>
-    )
-
-}
-
-export default SelectLocationComponent
+export default SelectLocationComponent;
--- flight-reservation/components/SelectLocationComponent/styles.js
+++ flight-reservation/components/SelectLocationComponent/styles.js
@@ -1,25 +1,25 @@
 import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
-    container:{
-        width: '90%',
-    },
-    boxStyles:{
-        width: '100%',
-        borderWidth: 0
-    },
-    optionsBox:{
-        borderWidth: 0
-    },
-    searchText: {
-        color: 'grey',
-    },
-    line:{
-        borderWidth: 0.5,
-        borderColor: 'grey',
-        width: '90%',
-        alignSelf: 'center'
-    }
+  container: {
+    width: "90%",
+  },
+  boxStyles: {
+    width: "100%",
+    borderWidth: 0,
+  },
+  optionsBox: {
+    borderWidth: 0,
+  },
+  searchText: {
+    color: "grey",
+  },
+  line: {
+    borderWidth: 0.5,
+    borderColor: "grey",
+    width: "90%",
+    alignSelf: "center",
+  },
 });
 
 export default styles;
