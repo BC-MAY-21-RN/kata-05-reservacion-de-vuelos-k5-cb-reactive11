3916589bbf3bc6b1437ae562641c124d27b8d52a
GerardBR3270 <lugo044@gmail.com>
2022-09-14 16:32:33 -0500

    feat(DatePicker): implement modern date DatePicker
    Co-authored-by: wiltrouble <wiltrouble@gmail.com>

--- flight-reservation/components/DateTimePickerComponent/index.js
+++ flight-reservation/components/DateTimePickerComponent/index.js
@@ -1,62 +1,16 @@
-import { StyleSheet, Text, View, Button, Platform } from 'react-native'
-import React, { useState } from 'react'
-import DateTimePicker from '@react-native-community/datetimepicker';
+import { View } from "react-native";
+import React, { useState } from "react";
+import DatePicker from "react-native-modern-datepicker";
+import styles from "./styles";
 
 const DatePickerComponent = () => {
-  const [date, setDate] = useState(new Date());
-  const [mode, setMode] = useState('date');
-  const [show, setShow] = useState(true);
-
-  const onChange = (event, selectedDate) => {
-    const currentDate = selectedDate;
-    setShow(false);
-    setDate(currentDate);
-  };
-
-  const showMode = (currentMode) => {
-    if (Platform.OS === 'android') {
-      setShow(false);
-      // for iOS, add a button that closes the picker
-    }
-    setMode(currentMode);
-    setShow(true)
-  };
-
-  const showDatepicker = () => {
-    showMode('date');
-  };
-
-  const showTimepicker = () => {
-    showMode('time');
-  };
+  const [date, setDate] = useState("");
 
   return (
     <View style={styles.container}>
-      <DateTimePicker
-          testID="dateTimePicker"
-          value={date}
-          mode={mode}
-          is24Hour={true}
-          onChange={onChange}
-          display='spinner'
-        />
-        <Text style={styles.text}>Selected: {date.toLocaleString()}</Text>
+      <DatePicker mode="calendar" onSelectedChange={(date) => setDate(date)} />
     </View>
   );
-}
+};
 
-export default DatePickerComponent
-
-const styles = StyleSheet.create({
-  container: {
-     width: '100%',
-     justifyContent: 'center',
-     alignContent: 'center'
-     
-  },
-  text: {
-    backgroundColor: 'red',
-    alignTextContent: 'center',
-    textAlign: 'center',
-  }
-})
+export default DatePickerComponent;
